a439dcb66d0fa117ad287eb81ecde645ce5f4080
Ender Loc Phan <weintome@gmail.com>
2017-01-24 23:34:08 +0200

    Upgrade the search Engine and No return None

--- findshell.py
+++ findshell.py
@@ -10,6 +10,7 @@ import stat
 import pwd
 import time
 
+
 ##########################################################################
 #					SCAN SHELL TOOL	 							                                                 #
 #			       	CODED by Ender Phan						                                                     #
@@ -43,13 +44,16 @@ day = args.day
 type = args.type
 
 if not os.path.exists(dir):
-    print " Directory entered does not exist?"
+    print  "Directory entered does not exist?"
 else:
 
     def input_day(file_name):
 
-        if day is not None:
+        if day is None:
+            return oct(os.stat(file_name)[
+                       0])[-3:], pwd.getpwuid(os.stat(file_name).st_uid)[0], modi_date(file_name), file_name
 
+        else:
             hour = day * 24
             today = datetime.datetime.now()
             #someday = today - datetime.timedelta(hours=hour)
@@ -59,14 +63,10 @@ else:
             sub_time = today - file_date
 
             if (sub_time < datetime.timedelta(hours=hour)):
-                return oct(os.stat(file_name)[
+                print oct(os.stat(file_name)[
                            0])[-3:], pwd.getpwuid(os.stat(file_name).st_uid)[0], modi_date(file_name), file_name
-        # break;
-            # else:
 
-        else:
-            return oct(os.stat(file_name)[
-                       0])[-3:], pwd.getpwuid(os.stat(file_name).st_uid)[0], modi_date(file_name), file_name
+
         # return someday.strftime("%Y-%m-%d")
 
     def modi_date(file_name):  # file time
@@ -84,14 +84,12 @@ else:
 
         cross_file = open(dir_file, 'r')
         o_cross_file = cross_file.read()
-        #cross_file.close()
+
         for x in patternarray:
             matches = re.findall(x,o_cross_file)
-        #cross_file.close()
             if len(matches) != 0:
-
-                print input_day(dir_file)
-            #cross_file.close()
+                input_day(dir_file)
+        cross_file.close()
     def open_folder(dir_folder):  # Open Folder
 
         if type is not None:
@@ -108,7 +106,7 @@ else:
                     open_folder(file1)
     # Main()
     patternarray = ['eval', 'base64_decode' ,'gzinflate','fsockopen','pfsockopen','exec','system','passthru','stream_socket_client','preg_replace','iframe']
-    print "\nthe files listed below are might be the shell \n"
+    print "\n----------------------------------------------------------- \n"
 
 
     #for x in patternarray:

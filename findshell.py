2945446194200368368872640286a9942c211c3d
Ender Loc Phan <weintome@gmail.com>
2017-01-24 21:41:47 +0200

    Update for pattern and Re.findall

--- findshell.py
+++ findshell.py
@@ -10,105 +10,122 @@ import stat
 import pwd
 import time
 
-##################################################################################################################
-#					SCAN SHELL TOOL	 							 #	
-#			       	CODED by Big-Small Creep						         #
-#               CODING WILL ChANGE THE WORLD - OTHERWISE IT COULD CHANGE YOUR MIND             			 #
-#										                                 #
-##################################################################################################################
+##########################################################################
+#					SCAN SHELL TOOL	 							                                                 #
+#			       	CODED by Ender Phan						                                                     #
+#               CODING WILL ChANGE THE WORLD - OTHERWISE IT COULD CHANGE YOUR MIND             			         #
+#										                                                                         #
+##########################################################################
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-D','--dir',help='Input Directory You Want To Scan',required=True)
-parser.add_argument('-d','--day',type=int,help='You can choose what date you want to scan util today',required=False)
-parser.add_argument('-t','--type',help="Chose file types you want to scan or blank if you want to scan fully (e.g: .php, .html) ",required=False)
+parser.add_argument(
+    '-D',
+    '--dir',
+    help='Input Directory You Want To Scan',
+    required=True)
+parser.add_argument(
+    '-d',
+    '--day',
+    type=int,
+    help='You can choose what date you want to scan util today',
+    required=False)
+parser.add_argument(
+    '-t',
+    '--type',
+    help="Chose file types you want to scan or blank if you want to scan fully (e.g: .php, .html) ",
+    required=False)
 
-args =parser.parse_args()
+args = parser.parse_args()
 
 dir = args.dir
 day = args.day
-type= args.type
+type = args.type
 
 if not os.path.exists(dir):
-        print " Directory entered does not exist?"
+    print " Directory entered does not exist?"
 else:
-	
-	def input_day(file_name):
-		
-		if day is not None:
-			
-			hour = day * 24
-			today =  datetime.datetime.now()	
-			#someday = today - datetime.timedelta(hours=hour)
-			file_date = datetime.datetime.utcfromtimestamp(os.path.getmtime(file_name))
-			
-			sub_time = today - file_date
-			
-			if (sub_time < datetime.timedelta(hours=hour)):
-				return oct(os.stat(file_name)[0])[-3:],pwd.getpwuid(os.stat(file_name).st_uid)[0],modi_date(file_name),file_name
-				
-			else:
-				return "";
-		else:
-			return oct(os.stat(file_name)[0])[-3:],pwd.getpwuid(os.stat(file_name).st_uid)[0],modi_date(file_name),file_name
-		#return someday.strftime("%Y-%m-%d")		
 
-	def modi_date(file_name): # file time
+    def input_day(file_name):
 
-		return time.strftime("%Y-%m-%d", time.gmtime(os.path.getmtime(file_name)))
+        if day is not None:
 
-	
-	def own(file_name):       #file permission
-		 
-		return getpwuid(stat(file_name).st_uid).pw_name
+            hour = day * 24
+            today = datetime.datetime.now()
+            #someday = today - datetime.timedelta(hours=hour)
+            file_date = datetime.datetime.utcfromtimestamp(
+                os.path.getmtime(file_name))
 
+            sub_time = today - file_date
 
-	def open_file(dir_file): #Open file
-		
-       		cross_file = open(dir_file,'r')
-        	o_cross_file = cross_file.read()
-        	cross_file.close()
-        	matches = filenames.findall(o_cross_file)
-		if len(matches) != 0:
-			#print oct(os.stat(dir_file)[0])[-3:],pwd.getpwuid(os.stat(dir_file).st_uid)[0],input_day(dir_file),matches
-			print input_day(dir_file)
-			#randay =  modi_date(dir_file) + datetime.date.today()
-			#print randay.days
-			#print datetime.date.today()
-	def open_folder(dir_folder): #Open Folder
+            if (sub_time < datetime.timedelta(hours=hour)):
+                return oct(os.stat(file_name)[
+                           0])[-3:], pwd.getpwuid(os.stat(file_name).st_uid)[0], modi_date(file_name), file_name
+        # break;
+            # else:
 
-		if type is not None:
-			for file1 in glob.glob(dir_folder+"/*"+type):
-				if os.path.isfile(file1):
-                        		open_file(file1)		                
-				if os.path.isdir(file1):		
-                       			open_folder(file1)
-		else:
-			for file1 in glob.glob(dir_folder+"/*"):
-                                if os.path.isfile(file1):
-                                        open_file(file1)
-                                if os.path.isdir(file1):
-                                        open_folder(file1)
-	# Main()	
-	patternarray = ['cmd', 'ender' ,'hello']
-	print "\nthe files listed below are might be the shell \n"
+        else:
+            return oct(os.stat(file_name)[
+                       0])[-3:], pwd.getpwuid(os.stat(file_name).st_uid)[0], modi_date(file_name), file_name
+        # return someday.strftime("%Y-%m-%d")
 
+    def modi_date(file_name):  # file time
+
+        return time.strftime(
+            "%Y-%m-%d",
+            time.gmtime(
+                os.path.getmtime(file_name)))
+
+    def own(file_name):  # file permission
+
+        return getpwuid(stat(file_name).st_uid).pw_name
+
+    def open_file(dir_file):  # Open file
+
+        cross_file = open(dir_file, 'r')
+        o_cross_file = cross_file.read()
+        #cross_file.close()
         for x in patternarray:
+            matches = re.findall(x,o_cross_file)
+        #cross_file.close()
+            if len(matches) != 0:
 
-		filenames = re.compile(r""+x)
-		if type is not None:
-                
-                	for file in glob.glob(dir+"/*"+type):
+                print input_day(dir_file)
+            #cross_file.close()
+    def open_folder(dir_folder):  # Open Folder
 
-                       		if os.path.isfile(file):
-                               		open_file(file)
-                       		if os.path.isdir(file):
-                               		open_folder(file)
-		else:
-			for file in glob.glob(dir+"/*"):
+        if type is not None:
+            for file1 in glob.glob(dir_folder + "/*" + type):
+                if os.path.isfile(file1):
+                    open_file(file1)
+                if os.path.isdir(file1):
+                    open_folder(file1)
+        else:
+            for file1 in glob.glob(dir_folder + "/*"):
+                if os.path.isfile(file1):
+                    open_file(file1)
+                if os.path.isdir(file1):
+                    open_folder(file1)
+    # Main()
+    patternarray = ['eval', 'base64_decode' ,'gzinflate','fsockopen','pfsockopen','exec','system','passthru','stream_socket_client','preg_replace','iframe']
+    print "\nthe files listed below are might be the shell \n"
 
-                                if os.path.isfile(file):
-                                        open_file(file)
-                                if os.path.isdir(file):
-                                        open_folder(file)
 
+    #for x in patternarray:
+
+        #filenames = re.compile(r"" + x)
+    if type is not None:
+
+        for file in glob.glob(dir + "/*" + type):
+
+            if os.path.isfile(file):
+                open_file(file)
+            if os.path.isdir(file):
+                open_folder(file)
+    else:
+        for file in glob.glob(dir + "/*"):
+
+            if os.path.isfile(file):
+                open_file(file)
+            if os.path.isdir(file):
+                open_folder(file)
